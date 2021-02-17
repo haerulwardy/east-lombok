ff3077ad41a7f06a291305556acc574e74bf6c69
haerulwardy <heydicreative@gmail.com>
2021-02-17 19:11:05 +0700

    membuat sidebar list admin

--- pages/admin/pages/list-admin/script.js
+++ pages/admin/pages/list-admin/script.js
@@ -15,6 +15,30 @@ closeAdmin.addEventListener('click', function() {
     modalAdmin.classList.add('invisible')
 })
 
+// sidebar
+const sidebarIcon = document.getElementById('sidebar-icon')
+const menuSidebar = document.getElementById('menu-sidebar')
+const closeSidebar = document.getElementById('close-sidebar')
+const sidebar = document.getElementById('sidebar')
+const menubar = document.querySelectorAll('.menu-bar')
+const logo = document.getElementById('logo')
+const adminContainer = document.getElementById('admin-container')
+
+sidebarIcon.addEventListener('click', function() {
+    sidebar.classList.toggle('w-1/4')
+    menuSidebar.classList.toggle('hidden')
+    closeSidebar.classList.toggle('hidden')
+    menubar.forEach(menu => {
+        menu.classList.toggle('hidden')
+    })
+    logo.classList.toggle('w-14')
+    logo.classList.toggle('pt-10')
+    adminContainer.classList.toggle('w-custom-sidebar')
+    adminContainer.classList.toggle('w-full')
+    adminContainer.classList.toggle('ml-96')
+    adminContainer.classList.toggle('ml-20')
+})
+
 
 
 
