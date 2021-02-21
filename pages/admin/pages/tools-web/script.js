50d737751fe2d3764396148e4ddd315f4911d6ec
haerulwardy <heydicreative@gmail.com>
2021-02-22 06:31:08 +0700

    menambahkan edit untuk tools cli dan web pages

--- pages/admin/pages/tools-web/script.js
+++ pages/admin/pages/tools-web/script.js
@@ -1,6 +1,5 @@
 const toolswebContent = document.getElementById('toolsweb-content')
 const addwebBtn = document.getElementById('add-web-btn')
-const editWeb = document.getElementById('edit-web')
 const modalWeb = document.getElementById('modal-web')
 const closeWeb = document.getElementById('close-web')
 
@@ -8,9 +7,6 @@ const closeWeb = document.getElementById('close-web')
 addwebBtn.addEventListener('click', function() {
     modalWeb.classList.toggle('invisible')
 })
-editWeb.addEventListener('click', function() {
-    modalWeb.classList.toggle('invisible')
-}) 
 closeWeb.addEventListener('click', function() {
     modalWeb.classList.toggle('invisible')
 })
