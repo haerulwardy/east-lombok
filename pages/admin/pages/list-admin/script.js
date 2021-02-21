8c1be70844209556c0b442a4164577a18656c19b
haerulwardy <heydicreative@gmail.com>
2021-02-22 06:11:53 +0700

    memperbaiki edit list user dan list admin

--- pages/admin/pages/list-admin/script.js
+++ pages/admin/pages/list-admin/script.js
@@ -1,14 +1,10 @@
 const listadminContent = document.getElementById('listadmin-content')
 const btnAdmin = document.getElementById('add-admin-btn')
-const editAdmin = document.getElementById('edit-admin')
 const modalAdmin = document.getElementById('modal-admin')
 const closeAdmin = document.getElementById('close-admin')
 
 // event
 btnAdmin.addEventListener('click', function() {
-    modalAdmin.classList.toggle('invisible')
-})
-editAdmin.addEventListener('click', function() {
     modalAdmin.classList.toggle('invisible')
 })
 closeAdmin.addEventListener('click', function() {
--- pages/admin/pages/list-user/script.js
+++ pages/admin/pages/list-user/script.js
@@ -1,14 +1,10 @@
 const listuserContent = document.getElementById('listuser-content')
 const btnUser = document.getElementById('add-user-btn')
-const editUser = document.getElementById('edit-user')
 const modalUser = document.getElementById('modal-user')
 const closeUser = document.getElementById('close-user')
 
 // event
 btnUser.addEventListener('click', function() {
-    modalUser.classList.toggle('invisible')
-})
-editUser.addEventListener('click', function() {
     modalUser.classList.toggle('invisible')
 })
 closeUser.addEventListener('click', function() {
